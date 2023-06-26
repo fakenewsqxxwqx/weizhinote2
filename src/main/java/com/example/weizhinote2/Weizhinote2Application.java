bb65c30b095b5074287884b7d49b4da3f15ba43a
yjx <1060710976@qq.com>
2023-06-26 09:08:51 +0800

    数据库连接测试完成

--- src/main/java/com/example/weizhinote2/Weizhinote2Application.java
+++ src/main/java/com/example/weizhinote2/Weizhinote2Application.java
@@ -1,9 +1,11 @@
 package com.example.weizhinote2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.weizhinote2.mapper")
 public class Weizhinote2Application {
 
     public static void main(String[] args) {
--- src/test/java/com/example/weizhinote2/Weizhinote2ApplicationTests.java
+++ src/test/java/com/example/weizhinote2/Weizhinote2ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.weizhinote2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class Weizhinote2ApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        int result=jdbcTemplate.queryForObject("SELECT 2",Integer.class);
+        System.out.println(result);
+        Assertions.assertEquals(2,result);
     }
-
 }
